a153dc4212e9351db2a83c576bf1bc91f81fa422
landyndigger-ctrl <landyndigger@gmail.com>
2026-01-03 16:04:37 -0800

    Delete anti-inspect.js

--- anti-inspect.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('contextmenu', e => e.preventDefault());
-document.onkeydown = e => {
-  if (
-    e.key === 'F12' ||
-    (e.ctrlKey && e.shiftKey && ['I','C','J'].includes(e.key)) ||
-    (e.ctrlKey && e.key === 'U')
-  ) return false;
-};
